6f3ca770f183c3ed86c4ff002806b0a9b474c409
ETU\l.bertaux <h4ck3r59@hotmail.fr>
2018-01-26 23:50:18 +0100

    ArenaManager Setup Spawn and spawn players - Exit behaviour - GameManager (need to ask to each player to press "A" to enter the game)

--- Assets/Scripts/ArenaManager.cs
+++ Assets/Scripts/ArenaManager.cs
@@ -1,8 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ArenaManager : MonoBehaviour {
+public class ArenaManager : MonoBehaviour
+{
+    public Transform spawnsParent;
+    public Transform charactersParent;
 
-	
+    public GameObject SpawnPrefab;
+
+    public GameObject characterPrefab;
+
+    private GameObject[] spawns;
+
+    private void Awake()
+    {
+        SetupSpawns(3);
+    }
+
+    /// <summary>
+    /// Setup a certain amount of spawns.
+    /// </summary>
+    /// <param name="numberOfPlayers"></param>
+    public void SetupSpawns(int numberOfPlayers)
+    {
+        // TODO Spawn as much characters as numbers of players.
+
+        spawns = new GameObject[numberOfPlayers];
+
+        float turnAngle = 360f / spawns.Length;
+
+        for (int i = 0; i < spawns.Length; i++)
+        {
+            spawns[i] = Instantiate(SpawnPrefab, Vector3.right * -10f, SpawnPrefab.transform.rotation, spawnsParent);
+            
+            spawns[i].transform.RotateAround(Vector3.zero, Vector3.up, turnAngle * i);
+        }
+    }
+   
+    /// <summary>
+    /// Spawn all players.
+    /// </summary>
+    /// <param name="numberOfPlayers"></param>
+    public void SpawnCharacters()
+    {
+        foreach(GameObject spawn in spawns)
+        {
+            GameObject character = Instantiate(characterPrefab, spawn.transform.position, Quaternion.identity, charactersParent);
+
+            // Orientate to exit.
+            character.transform.LookAt(Vector3.zero);
+        }
+    }
+
+    // Trigger win !
+    public void TriggerWin(int winnerIndex)
+    {
+        // TODO Stop all players
+
+        // DISPLAY WINNER.
+
+        Debug.Log("Player " + winnerIndex + " wins !");
+    }
 }
