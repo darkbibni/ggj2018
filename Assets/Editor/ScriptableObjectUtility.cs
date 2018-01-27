2ca6b65ed0974109620ba3ba2d0b74175be7b142
Steve Delgrange <s.delgrange@rubika-edu.com>
2018-01-27 11:07:16 +0100

    Added scriptable object for skills.

---
+++ Assets/Editor/ScriptableObjectUtility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class ScriptableObjectUtility
+{
+    /// <summary>
+    //    This makes it easy to create, name and place unique new ScriptableObject asset files.
+    /// </summary>
+    public static void CreateAsset<T>(string extraPath = "", string fileName = null) where T : ScriptableObject
+    {
+        T asset = ScriptableObject.CreateInstance<T>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+            path = "Assets";
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        string assetPathAndName = null;
+        if (extraPath.Equals("") && fileName == null)
+            assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/ New " + typeof(T).ToString() + ".asset");
+        else if (fileName != null)
+            assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + extraPath + "/" + fileName + ".asset");
+
+        AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/Editor/SkillUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SkillUtility : MonoBehaviour {
+
+    [MenuItem("Assets/Create/ScriptableObjects/Gun_Data")]
+    public static void CreateGunDataAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<Gun_Data>("/Data", "Gun_Data");
+    }
+}
